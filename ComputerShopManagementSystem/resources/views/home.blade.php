6bc8211e0279f466bb5962b2a29bb5c4315720af
JowenGonzales <gonzalesmj0942@gmail.com>
2023-03-26 23:19:40 +0800

    Update home.blade.php

--- ComputerShopManagementSystem/resources/views/home.blade.php
+++ ComputerShopManagementSystem/resources/views/home.blade.php
@@ -38,16 +38,113 @@
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                aria-expanded="true" aria-controls="collapseTwo">
                 <i class="fas fa-fw fa-cog"></i>
-                <span>Components</span>
+                <span>Users</span>
             </a>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom Components:</h6>
-                    <a class="collapse-item" href="buttons.html">Buttons</a>
-                    <a class="collapse-item" href="cards.html">Cards</a>
+                    <h6 class="collapse-header">Manage Users:</h6>
+                    <a class="collapse-item" href="buttons.html">Add User </a>
+                    <a class="collapse-item" href="cards.html">Edit User  </a>
+                    <a class="collapse-item" href="cards.html">Delete User</a>
+                    <a class="collapse-item" href="cards.html">Manage User Roles</a>
+                    <a class="collapse-item" href="cards.html">User's Usage Statistics</a>
+                    <a class="collapse-item" href="cards.html">User's Billing Information</a>
                 </div>
             </div>
         </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Computers</span>
+            </a>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Manage Computers:</h6>
+                    <a class="collapse-item" href="buttons.html">Add</a>
+                    <a class="collapse-item" href="cards.html">Edit Configuration  </a>
+                    <a class="collapse-item" href="cards.html">Usage Statistics</a>
+                    <a class="collapse-item" href="cards.html">Maintenance Logs</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Pricing and Packages</span>
+            </a>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Pricing and Packages:</h6>
+                    <a class="collapse-item" href="buttons.html">Set Pricing </a>
+                    <a class="collapse-item" href="cards.html">Create Packages  </a>
+                    <a class="collapse-item" href="cards.html">Manage Promotions</a>
+                    <a class="collapse-item" href="cards.html">Monitor Revenue </a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFive"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Reservation</span>
+            </a>
+            <div id="collapseFive" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Reservations:</h6>
+                    <a class="collapse-item" href="buttons.html">Adding New </a>
+                    <a class="collapse-item" href="cards.html">Edit Existing  </a>
+                    <a class="collapse-item" href="cards.html">View Details</a>
+                    <a class="collapse-item" href="cards.html">Listing of All </a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSix"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Sales and Reports</span>
+            </a>
+            <div id="collapseSix" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Sales and Reports</h6>
+                    <a class="collapse-item" href="buttons.html">View Sales Reports</a>
+                    <a class="collapse-item" href="cards.html">Analyze Usage Statistics</a>
+                    <a class="collapse-item" href="cards.html">Monitor User Satisfaction</a>
+                    <a class="collapse-item" href="cards.html">Identify Trends</a>
+                </div>
+            </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSeven"
+               aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Employees</span>
+            </a>
+            <div id="collapseSeven" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Manage Employees:</h6>
+                    <a class="collapse-item" href="buttons.html">Manage Schedules</a>
+                    <a class="collapse-item" href="cards.html">Attendance & Payroll</a>
+                    <a class="collapse-item" href="cards.html">Monitor Performance</a>
+                    <a class="collapse-item" href="cards.html">Set Up Training Programs</a>
+                </div>
+            </div>
+        </li>
+        
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Settings
+        </div>
 
         <!-- Nav Item - Utilities Collapse Menu -->
         <li class="nav-item">
@@ -67,64 +164,6 @@
                 </div>
             </div>
         </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider">
-
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Addons
-        </div>
-
-        <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-               aria-expanded="true" aria-controls="collapsePages">
-                <i class="fas fa-fw fa-folder"></i>
-                <span>Pages</span>
-            </a>
-            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Login Screens:</h6>
-                    <a class="collapse-item" href="login.html">Login</a>
-                    <a class="collapse-item" href="register.html">Register</a>
-                    <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                    <div class="collapse-divider"></div>
-                    <h6 class="collapse-header">Other Pages:</h6>
-                    <a class="collapse-item" href="404.html">404 Page</a>
-                    <a class="collapse-item" href="blank.html">Blank Page</a>
-                </div>
-            </div>
-        </li>
-
-        <!-- Nav Item - Charts -->
-        <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw fa-chart-area"></i>
-                <span>Charts</span></a>
-        </li>
-
-        <!-- Nav Item - Tables -->
-        <li class="nav-item">
-            <a class="nav-link" href="tables.html">
-                <i class="fas fa-fw fa-table"></i>
-                <span>Tables</span></a>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
-
-        <!-- Sidebar Toggler (Sidebar) -->
-        <div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-        </div>
-
-        <!-- Sidebar Message -->
-        <div class="sidebar-card d-none d-lg-flex">
-            <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-            <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-            <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-        </div>
 
     </ul>
     <!-- End of Sidebar -->
