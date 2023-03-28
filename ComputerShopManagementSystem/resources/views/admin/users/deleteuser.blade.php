200f13bdafd8f01ba65d2ab360ddc2a0155d7cfb
JowenGonzales <gonzalesmj0942@gmail.com>
2023-03-28 22:59:07 +0800

    deleteuser

--- ComputerShopManagementSystem/resources/views/admin/users/deleteuser.blade.php
+++ ComputerShopManagementSystem/resources/views/admin/users/deleteuser.blade.php
@@ -2,7 +2,33 @@
 @section('page-title', 'Delete User')
 @section('content')
     <div class="">
+        <!-- Button trigger modal -->
 
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure you want to delete this user?
+                        <p>ID: <span class="useridtext text-primary" ></span></p>
+                        <input type="hidden" id="userid" name="userid" class="userid">
+                    </div>
+                    <div class="modal-footer">
+                        <form>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
@@ -33,7 +59,10 @@
                                 <td>{{$customer->id}}</td>
                                 <td>{{$customer->name}}</td>
                                 <td>
-                                    <i class="bi bi-pen"></i>
+
+                                    <button type="button" class="btn btn-primary actiondelete" data-toggle="modal" data-target="#exampleModal" id="actiondelete" data-user-id="{{$customer->id}}">
+                                        <i class="bi bi-pen"></i>
+                                    </button>
                                 </td>
                             </tr>
                         @endforeach
@@ -54,7 +83,15 @@
 @endsection
 
 @section('body-scripts')
-
+    <script>
+        $(document).ready(function() {
+           $('.actiondelete').click(function () {
+                var userID = $(this).data('user-id');
+                $('.useridtext').text(userID);
+                $('#userid').val(userID);
+           });
+        });
+    </script>
     <!-- Page level plugins -->
     <script src="{{asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
