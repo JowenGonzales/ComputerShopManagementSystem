0f14e67f65971c5c927d1eb44a1e2a859cba8502
JowenGonzales <gonzalesmj0942@gmail.com>
2023-03-31 22:30:25 +0800

    Update editcomputer.blade.php
    An update has been made to the editcomputer.blade.php file, which is responsible for handling the display and editing of a computer's information. This update includes various enhancements to improve the user experience and streamline the editing process.

--- ComputerShopManagementSystem/resources/views/admin/computers/editcomputer/editcomputer.blade.php
+++ ComputerShopManagementSystem/resources/views/admin/computers/editcomputer/editcomputer.blade.php
@@ -14,17 +14,34 @@
             <div class="col-xl-4">
                 <!-- Profile picture card-->
                 <div class="card mb-4 mb-xl-0">
-                    <div class="card-header">Profile Picture</div>
-                    <div class="card-body text-center">
-                        <!-- Profile picture image-->
-                        <img class="img-account-profile rounded-circle mb-2" src="https://sb-admin-pro.startbootstrap.com/assets/img/illustrations/profiles/profile-1.png" alt="">
-                        <!-- Profile picture help block-->
-                        <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
-                        <!-- Profile picture upload button-->
-                        <button class="btn btn-primary" type="button">Upload new image</button>
+                    <div class="card-header">Information</div>
+                    <div class="card-body ">
+                        <div>
+                            <p>Status: <span>{{$computer->name}}</span></p>
+                        </div>
+
+                        <div>
+                            <p>End Time: <span>{{$computer->name}}</span></p>
+                        </div>
+
+                        <div>
+                            <p>Total Time Used: <span>{{$computer->name}}</span></p>
+                        </div>
+
+                        <div>
+                            <p>Last User: <span>{{$computer->name}}</span></p>
+                        </div>
+
+                        <div>
+                            <p>Last Used Time: <span>{{$computer->name}}</span></p>
+                        </div>
+
+
                     </div>
                 </div>
             </div>
+
+
             <div class="col-xl-8">
                 <!-- Account details card-->
                 <div class="card mb-4">
@@ -37,30 +54,50 @@
                                 <label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
                                 <input name="name" class="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="{{$computer->name}}">
                             </div>
-                            <!-- Form Row-->
 
                             <div class="mb-3">
-                                <label class="small mb-1" for="inputUsername">Status</label>
+                                <label class="small mb-1" for="inputUsername">Price Per Hour</label>
                                 <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
                             </div>
 
                             <div class="mb-3">
-                                <label class="small mb-1" for="inputUsername">Computer</label>
+                                <label class="small mb-1" for="inputUsername">Total Price</label>
                                 <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
                             </div>
 
                             <div class="mb-3">
-                                <label class="small mb-1" for="inputUsername">Computer</label>
+                                <label class="small mb-1" for="inputUsername">Operating System</label>
                                 <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
                             </div>
 
                             <div class="mb-3">
-                                <label class="small mb-1" for="inputUsername">Computer</label>
+                                <label class="small mb-1" for="inputUsername">Processor </label>
                                 <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
                             </div>
 
+                            <div class="mb-3">
+                                <label class="small mb-1" for="inputUsername">Ram</label>
+                                <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
+                            </div>
 
+                            <div class="mb-3">
+                                <label class="small mb-1" for="inputUsername">Graphics Card</label>
+                                <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
+                            </div>
 
+                            <div class="mb-3">
+                                <label class="small mb-1" for="inputUsername">Storage</label>
+                                <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="small mb-1" for="inputUsername">Location</label>
+                                <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
+                            </div>
+                            <div class="mb-3">
+                                <label class="small mb-1" for="inputUsername">Notes</label>
+                                <input name="name" class="form-control" id="inputUsername" type="text" placeholder="" value="{{$computer->name}}">
+                            </div>
 
 
                             <!-- Form Row-->
